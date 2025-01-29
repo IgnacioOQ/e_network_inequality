3d66212bdc638275af185b43f9b46ba5e5e13695
IgnacioOQ <ignacioojea@gmail.com>
2025-01-29 14:59:22 +0100

    twick randomization function

--- network_randomization.py
+++ network_randomization.py
@@ -36,3 +36,41 @@ def randomize_network(G, p_rewiring):
     G_new.add_edges_from(new_edges_set)
 
     return G_new
+
+def randomize_networkv2(G, p_rewiring):
+    # Check if the graph is directed
+    is_directed = G.is_directed()
+
+    # Get edges and nodes
+    edges = list(G.edges()).copy()
+    random.shuffle(edges)
+    edges_set = set(edges)
+    new_edges_set = edges_set.copy()
+    nodes = list(G.nodes()).copy()
+
+    to_remove_set = set()
+    for old_edge in edges:
+        if random.random() < p_rewiring:  # p probability to rewire an edge
+            to_remove_set.add(old_edge)
+            new_edges_set.remove(old_edge)
+
+    # Generate a new edges
+    for edge in to_remove_set:
+        new_edge = (random.choice(nodes), random.choice(nodes))
+        if not is_directed:
+            new_edge = tuple(sorted(new_edge))  # Ensure (u, v) == (v, u) for undirected graphs
+
+        # Avoid duplicate edges and self-loops
+        while (new_edge in new_edges_set) or (new_edge[0] == new_edge[1]):
+            new_edge = (random.choice(nodes), random.choice(nodes))
+            if not is_directed:
+                new_edge = tuple(sorted(new_edge))
+
+        new_edges_set.add(new_edge)
+
+    # Create a new graph with updated edges
+    G_new = G.copy()
+    G_new.remove_edges_from(to_remove_set)
+    G_new.add_edges_from(new_edges_set)
+
+    return G_new
