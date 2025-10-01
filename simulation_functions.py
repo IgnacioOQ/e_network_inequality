0d769e05f4a0287454ffdb379a555bbfab7ec1f9
IgnacioOQ <ignacioojea@gmail.com>
2025-10-01 17:37:32 +0200

    simplifying the output dictionary by removing large lists that are not necessary for analysis

--- simulation_functions.py
+++ simulation_functions.py
@@ -105,8 +105,8 @@ def run_simulation_with_params(param_dict, tolerance = 5*1e-03,seed=420,seeded=F
 
     result_dict['share_of_correct_agents_at_convergence'] = my_model.conclusion
     result_dict['convergence_step'] = my_model.n_steps # takes note of the last reported step
-    result_dict['init_agents_alphas_betas'] = my_model.init_agents_alphas_betas
-    result_dict['degree_centrality_vector'] = my_model.degree_centrality_vector
+    # result_dict['init_agents_alphas_betas'] = my_model.init_agents_alphas_betas
+    # result_dict['degree_centrality_vector'] = my_model.degree_centrality_vector
     result_dict['simulation_seed'] = process_seed
     result_dict['rootnode_influence'] = my_model.rootnode_influence
     if 'group_id' in param_dict:
