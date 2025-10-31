6847155ca804b8ec25c62f6b8a341cbc928e8580
IgnacioOQ <ignacioojea@gmail.com>
2025-10-31 19:23:14 +0100

    tstep_stopping

--- simulation_functions.py
+++ simulation_functions.py
@@ -83,7 +83,8 @@ def generate_parameters_aggregate(G=G_default,uncertainty=0.005,n_experiments=20
 
     return params
 
-def run_simulation_with_params(param_dict, tolerance = 5*1e-03,seed=420,seeded=False, number_of_steps=20000, show_bar=False):
+def run_simulation_with_params(param_dict, tolerance = 5*1e-03, tstep_stopping=False,
+                               seed=420,seeded=False, number_of_steps=20000, show_bar=False):
     
     process_seed = int.from_bytes(os.urandom(4), byteorder='little')
     rd.seed(process_seed)
@@ -93,7 +94,8 @@ def run_simulation_with_params(param_dict, tolerance = 5*1e-03,seed=420,seeded=F
     # Other parameters are directly extracted from the dictionary
     my_model = Model(my_network, n_experiments=param_dict['n_experiments'],
                     uncertainty=param_dict['uncertainty'],tolerance = tolerance,
-                    histories=False,sampling_update=False,variance_stopping = False,directed_network = True,
+                    histories=False,sampling_update=False,variance_stopping = False,
+                    tstep_stopping = tstep_stopping,directed_network = True,
                     seed=420,seeded=False, agent_class=BetaAgent)
     # Run the simulation with predefined steps and show_bar option
 
