dd910266f01548ee8f77b81c2b39e931a0ebd635
HeinDuijf <duijf.hein@gmail.com>
2026-02-26 14:33:05 +0100

    reordereed and changed tqdm to from tqdm.auto import tqdm

--- src/net_epistemology/utils/imports.py
+++ src/net_epistemology/utils/imports.py
@@ -1,22 +1,23 @@
+import copy
+import hashlib
+import os
+import pickle
+import random
+import unittest
+import uuid
+from multiprocessing import Pool, cpu_count
+
+import dill
+import matplotlib.pyplot as plt
+import networkx as nx
 import numpy as np
 import numpy.random as rd
+import pandas as pd
+import scipy.linalg
+import seaborn as sns
 
 # rd = np.random.default_rng()
 from scipy.stats import beta
-import tqdm
-import pickle
-import pandas as pd
-import networkx as nx
-import random
-import matplotlib.pyplot as plt
-import seaborn as sns
-import uuid
-import os
-from multiprocessing import Pool, cpu_count
-import unittest
-import dill
-import copy
-import hashlib
-import scipy.linalg
+from tqdm.auto import tqdm
 
 # import graph_tool.all as gt
