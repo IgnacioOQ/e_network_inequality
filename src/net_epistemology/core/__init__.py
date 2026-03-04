13e4d7a6319b94f64d239b340fd1290582815228
IgnacioOQ <ignacioojea@gmail.com>
2026-03-04 15:27:09 -0300

    refactor: Relocate `VectorizedBandit` class from `vectorized_agents.py` to a new `bandit.py` module.

--- src/net_epistemology/core/__init__.py
+++ src/net_epistemology/core/__init__.py
@@ -1,4 +1,4 @@
 from .model import Model
 from .agents import Bandit, BetaAgent, BayesAgent
 from .vectorized_model import VectorizedModel
-from .vectorized_agents import VectorizedBandit
+from .bandit import VectorizedBandit
